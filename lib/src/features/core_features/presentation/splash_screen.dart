045354a741fe037d1a602a33c62a467ac4c1037b
dev-ajijul-islam <devajijulislam@gmail.com>
2026-04-29 09:40:46 +0600

    language selection menu added in splash screen

--- lib/src/features/core_features/presentation/splash_screen.dart
+++ lib/src/features/core_features/presentation/splash_screen.dart
@@ -7,6 +7,7 @@ import 'package:riverpod_tamplates/config/constance/constants.dart';
 import 'package:riverpod_tamplates/config/route/app_router.dart';
 import 'package:riverpod_tamplates/config/theme/app_theme_data.dart';
 import 'package:riverpod_tamplates/gen/assets.gen.dart';
+import 'package:riverpod_tamplates/src/common/language_selector_widget.dart';
 import 'package:riverpod_tamplates/src/features/core_features/authentication/data/auth_repository.dart';
 
 @RoutePage()
@@ -46,67 +47,78 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-   
+    return Scaffold(
       body: Container(
-        decoration: BoxDecoration(gradient: context.color.ctaGradientBackgroundAccent),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: Constants.padding),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [ 
-              CommonAppBar(
-                disableBack: true,
-                hideBack: true,
-                appbarConfig: AppbarConfig(
-                  decoration: () => const BoxDecoration(color: Colors.transparent),
-                ),
+        decoration: BoxDecoration(
+          gradient: context.color.ctaGradientBackgroundAccent,
+        ),
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Constants.padding,
               ),
-              const Spacer(),
-              CommonImage(
-                src: Assets.images.spalshIcon.path,
-                width: 207,
-                height: 232,
-                enableAspectRatio: true,
-              ).center,
-              32.height,
-              CommonText(
-                left: 10,
-                right: 10,
-                text: AppString.discover_thousands_of_stories_Read_anywhere_anytime,
-                style: context.textStyle.subTitle,
-                textColor: context.color.bgColor.withValues(alpha: 0.8),
-                isDescription: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).padding.top + 30),
+                  const Spacer(),
+                  CommonImage(
+                    src: Assets.images.spalshIcon.path,
+                    width: 207,
+                    height: 232,
+                    enableAspectRatio: true,
+                  ).center,
+                  32.height,
+                  CommonText(
+                    left: 10,
+                    right: 10,
+                    text: AppString
+                        .discover_thousands_of_stories_Read_anywhere_anytime,
+                    style: context.textStyle.subTitle,
+                    textColor: context.color.bgColor.withValues(alpha: 0.8),
+                    isDescription: true,
+                  ),
+                  const Spacer(),
+                  CommonButton(
+                    titleText: AppString.get_started,
+                    buttonColor: context.color.bgColor,
+                    titleGradient: context.color.ctaGradientLogo,
+                    onTap: () {
+                      appRouter.push(const OnboardingRoute());
+                    },
+                  ),
+                  16.height,
+                  CommonButton(
+                    titleText: AppString.i_already_have_an_account,
+                    buttonColor: context.color.buttonTextWhite.withValues(
+                      alpha: 0.1,
+                    ),
+                    borderColor: context.color.buttonTextWhite.withValues(
+                      alpha: 0.4,
+                    ),
+                    borderWidth: 2,
+                    onTap: () {
+                      appRouter.push(const LoginRoute());
+                    },
+                  ),
+                  24.height,
+                  CommonText(
+                    text: AppString.join_1M_readers_worldwide,
+                    style: context.textStyle.subTitle,
+                    fontSize: 14,
+                    textColor: context.color.bgColor.withValues(alpha: 0.8),
+                  ),
+                  const Spacer(),
+                ],
               ),
-              const Spacer(),
-              CommonButton(
-                titleText: AppString.get_started,
-                buttonColor: context.color.bgColor,
-                titleGradient: context.color.ctaGradientLogo,
-                onTap: () {
-                  appRouter.push(const OnboardingRoute());
-                },
-              ),
-              16.height,
-              CommonButton(
-                titleText: AppString.i_already_have_an_account,
-                buttonColor: context.color.buttonTextWhite.withValues(alpha: 0.1),
-                borderColor: context.color.buttonTextWhite.withValues(alpha: 0.4),
-                borderWidth: 2,
-                onTap: () {
-                  appRouter.push(const LoginRoute());
-                },
-              ),
-              24.height,
-              CommonText(
-                text: AppString.join_1M_readers_worldwide,
-                style: context.textStyle.subTitle,
-                fontSize: 14,
-                textColor: context.color.bgColor.withValues(alpha: 0.8),
-              ),
-              const Spacer(),
-            ],
-          ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).padding.top + 20,
+              right: 20,
+              child: const LanguageSelectorWidget(),
+            ),
+          ],
         ),
       ),
     );
